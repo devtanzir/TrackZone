69901cc8104db9fe883ab6d4aaf71c8e598e7566
tanzir ibne ali <tanziribneali@gmail.com>
2024-06-13 10:16:55 +0600

    mobile issue fixing

--- src/components/ui/AllFormItem.jsx
+++ src/components/ui/AllFormItem.jsx
@@ -4,6 +4,7 @@ export const Input = styled.input`
   display: block;
   padding: 0.75rem;
   border-radius: 0.5rem;
+  font-family: "Poppins", sans-serif;
   outline: none;
   font-size: 18px;
   border: ${(props) => (props.$error ? "2px solid #ff0000" : "2px solid #000")};
@@ -15,11 +16,16 @@ export const Input = styled.input`
   &:focus {
     border: 2px solid #535c68;
   }
+  @media (max-width: 768px) {
+    padding: 6px;
+    font-size: 16px;
+  }
 `;
 export const TextArea = styled.textarea`
   height: 90px;
   padding: 0.75rem;
   border-radius: 0.5rem;
+  font-family: "Poppins", sans-serif;
   resize: none;
   border: ${(props) => (props.$error ? "2px solid #ff0000" : "2px solid #000")};
   margin-bottom: ${(props) => (props.$error ? "" : "27px")};
@@ -31,6 +37,10 @@ export const TextArea = styled.textarea`
     drop-shadow(0 4px 3px rgba(0, 0, 0, 0.1));
   &:focus {
     border: 2px solid #535c68;
+  }
+  @media (max-width: 768px) {
+    padding: 6px;
+    font-size: 15px;
   }
 `;
 
@@ -56,6 +66,7 @@ export const InputWrapper = styled.div`
   gap: 17px;
 `;
 export const Label = styled.label`
+  font-family: "Poppins", sans-serif;
   display: block;
   margin-bottom: 6px;
 `;
@@ -67,6 +78,7 @@ export const SubmitBtn = styled.button`
   position: relative;
   padding-top: 0.625rem;
   padding-bottom: 0.625rem;
+  font-family: "Poppins", sans-serif;
   padding-left: 1.25rem;
   padding-right: 1.25rem;
   margin-top: 1.5rem;
@@ -88,17 +100,28 @@ export const Select = styled.select`
   padding: 7px;
   padding-right: 0;
   background-color: #dfe4ea3b;
+  font-family: "Poppins", sans-serif;
   border-radius: 6px;
   font-size: 19px;
   cursor: pointer;
   &:hover {
     background-color: #dfe4ea;
   }
+  @media (max-width: 768px) {
+    padding: 6px;
+    font-size: 16px;
+  }
 `;
 export const Option = styled.option`
+  font-family: "Poppins", sans-serif;
   background-color: #dfe4ea;
+  @media (max-width: 768px) {
+    padding: 6px;
+    font-size: 16px;
+  }
 `;
 export const Error = styled.div`
+  font-family: "Poppins", sans-serif;
   font-size: 1rem;
   color: red;
   margin-top: 7px;
